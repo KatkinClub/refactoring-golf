a688607d77778ed4936b4c4372f343af7d525d46
Reinholds Bunde <mrreinholds@gmail.com>
2021-05-20 15:12:09 +0100

    Adding some small changes to make it work for kotlin

--- hole1/src/test/java/tax/TakeHomeCalculatorTest.kt
+++ hole1/src/test/java/tax/TakeHomeCalculatorTest.kt
@@ -1,19 +1,27 @@
 package tax
 
-import org.junit.Assert
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+
 
 class TakeHomeCalculatorTest {
     @Test
     @Throws(Exception::class)
     fun canCalculateTax() {
-        val first = TakeHomeCalculator(10).netAmount(TakeHomeCalculator.Pair(40, "GBP"), TakeHomeCalculator.Pair(50, "GBP"), TakeHomeCalculator.Pair(60, "GBP")).first
-        Assert.assertEquals(135, first.toLong())
+        val first = TakeHomeCalculator(10).netAmount(
+            TakeHomeCalculator.Pair(40, "GBP"),
+            TakeHomeCalculator.Pair(50, "GBP"),
+            TakeHomeCalculator.Pair(60, "GBP")
+        ).first
+        assertEquals(135, first.toLong())
     }
 
-    @Test(expected = Incalculable::class)
+    @Test
     @Throws(Exception::class)
     fun cannotSumDifferentCurrencies() {
-        TakeHomeCalculator(10).netAmount(TakeHomeCalculator.Pair(4, "GBP"), TakeHomeCalculator.Pair(5, "USD"))
+        assertThrows(Incalculable::class.java) {
+            TakeHomeCalculator(10).netAmount(TakeHomeCalculator.Pair(4, "GBP"), TakeHomeCalculator.Pair(5, "USD"))
+        }
     }
 }
